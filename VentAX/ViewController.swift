94991bca37b9f5763706f0304b8bdf0eb965952e
Xchel Carranza <xchel.co@gmail.com>
2021-04-04 19:21:25 -0500

    Initial Commit

---
+++ VentAX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VentAX
+//
+//  Created by Xchel Alonso Carranza De La O on 04/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
